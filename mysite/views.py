1eef4291d76e0e01e921c5ad723c3de704510bd6
alireza-rezaeifard <alireza.rezaeifard13811@gmail.com>
2023-09-02 22:49:36 +0330

    pages
    just made a home and about and contact page for the website

--- mysite/views.py
+++
@@ -1,8 +0,0 @@
-
-from django.http import HttpResponse,JsonResponse
-
-def http_test(requests) :
-    return HttpResponse('<h1>http test succecfully</h1>')
-
-def json_test(requsts) : 
-    return JsonResponse({'name' : 'ali'})
--- website/views.py
+++ website/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def home_view(request) :
+    return HttpResponse('<h1>this is a home page view</h1>')
+
+def about_view(request) :
+    return HttpResponse('<h1>this is a about page view</h1>')
+
+def contact_view(request) :
+    return HttpResponse('<h1>this is a contact page view</h1>')
 
 # Create your views here.
