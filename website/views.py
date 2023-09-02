f0d3d45b46965eb654e7e042220be39bf02ec255
alireza-rezaeifard <alireza.rezaeifard13811@gmail.com>
2023-09-03 00:06:57 +0330

    add files into folders

--- website/views.py
+++ website/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index_view(request) :
-    return render(request, 'index.html')
+    return render(request, 'home_page/index.html')
 
 def about_view(request) :
-    return render(request, 'about.html')
+    return render(request, 'about_page/about.html')
 
 def contact_view(request) :
-    return render(request, 'contact.html')
+    return render(request, 'contact_page/contact.html')
 
 # Create your views here.
