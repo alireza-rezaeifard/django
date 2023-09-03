4bb931150b48e43ee2e7cb669877676f5f565874
alireza-rezaeifard <alireza.rezaeifard13811@gmail.com>
2023-09-03 13:55:38 +0330

    add a app name for pages

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,9 @@
 
 from django.urls import path
 from website.views import *
+
+app_name = 'website'
+
 urlpatterns = [
     path('', index_view, name = 'home'),
     path('about', about_view, name = 'about'),
