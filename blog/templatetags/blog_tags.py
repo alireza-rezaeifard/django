2b64409ac300f7f102bb06aea75f7a1255b5b76a
alireza-rezaeifard <alireza.rezaeifard13811@gmail.com>
2023-09-28 18:35:00 +0330

    update blog tags

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -2,3 +2,7 @@ from django import template
 
 register = template.Library()
 
+@register.inclusion_tag('blog/popular-post.html')
+def latestpost():
+    posts = posts.object.fillter(status=1).oreder_by('published_date')[:3]
+    return {'posts':posts}
