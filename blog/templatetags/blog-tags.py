f7b7e553d6af93e67499e36a0074e0c3c3829975
alireza-rezaeifard <alireza.rezaeifard13811@gmail.com>
2023-09-28 17:45:34 +0330

    creage template tags

--- blog/templatetags/blog-tags.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.inclusion_tag('blog/popular-post.html')
-def pupular():
-    posts = posts.object.fillter(status=1).oreder_by('published_date')[:3]
-    return {'posts':posts}
