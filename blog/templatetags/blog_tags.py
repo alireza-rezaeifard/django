4869d0825bc91b5ae87902cf63452593c696e27e
alireza-rezaeifard <alireza.rezaeifard13811@gmail.com>
2023-10-03 02:07:50 +0330

    add some feature
    -add popular post section to blog
    -add post category section to blog

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from blog.models import post
+from blog.models import post,category
 register = template.Library()
 
 @register.inclusion_tag('blog/popular-post.html')
@@ -7,3 +7,15 @@ def latestpost():
     global posts
     posts = post.objects.filter(status=1).order_by('published_date')[:4]
     return {'posts':posts}
+
+@register.inclusion_tag('blog/blog-post-category.html')
+def postcat():
+    posts = post.objects.filter(status=1)
+    categories = category.objects.all()
+    cat_dict = {}
+    
+    for name in categories :
+        cat_dict[name] = posts.filter(category=name).count()
+    
+    return {'categories':cat_dict}
+    
