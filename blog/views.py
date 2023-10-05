c31f0c06223011f389736d5c7afd365ab01ace95
alireza-rezaeifard <alireza.rezaeifard13811@gmail.com>
2023-10-05 19:49:37 +0330

    pagination
    apply pagination for the blog section of server

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,32 @@
 from django.shortcuts import get_object_or_404, render
 from blog.models import post
-
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 # Create your views here.
 def blog_view(request, cat_name=None, author_username=None) :
-    posts = post.objects.filter(status=1)    
+    posts = post.objects.filter(status=1)   
+    #for category fillter 
     if cat_name :
         posts = posts.filter(category__name=cat_name)
+    #for username author
     if author_username :
         posts = posts.filter(author__username=author_username)
+        
+    # for the pagination of the blog pages   
+    paginator = Paginator(posts, 4)
+    
+    try:
+        page_number = request.GET.get('page')
+        posts = paginator.get_page(page_number)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.get_page(1)
+    except EmptyPage:
+        posts = paginator.get_page(1)        
+    # end of pagination
+    
     context = {'posts':posts}
     return render(request, 'blog/blog-home.html',context)
-
+    
 def blog_single(request, pid):
     post_s = post.objects.filter(status=1)
     posts = get_object_or_404(post_s, pk=pid)
